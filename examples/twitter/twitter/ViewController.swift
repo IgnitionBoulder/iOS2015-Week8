1a1d7a8ff620ec82b26bc0b39fd0e86237c298a8
Eric Reid <reidek@gmail.com>
2015-11-17 19:54:52 -0700

    Fix twitter example

--- examples/twitter/twitter/ViewController.swift
+++ examples/twitter/twitter/ViewController.swift
@@ -11,9 +11,12 @@ import STTwitter
 
 class ViewController: UIViewController {
 
+    var api: STTwitterAPI!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        api = STTwitterAPI.twitterAPIOSWithFirstAccount()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,27 +24,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
     @IBAction func tweet(sender: AnyObject) {
-        let api = STTwitterAPI.twitterAPIOSWithFirstAccount()
-//        api.verifyCredentialsWithUserSuccessBlock({ (userName:String!, userID:String!) -> Void in
-//                print(userName)
-//            }) { (error:NSError!) -> Void in
-//                print(error)
-//        }
+        api.verifyCredentialsWithUserSuccessBlock({ (username:String!, userID:String!) -> Void in
+            self.getTweets()
+            }) { (error:NSError!) -> Void in
+            print(error)
+        }
+    }
 
-//        api.postStatusUpdate("This is a tweet", inReplyToStatusID: "", latitude: "", longitude: "", placeID: "", displayCoordinates: 0, trimUser: 0, successBlock: { (result:[NSObject : AnyObject]!) -> Void in
-//                print(result)
-//            }) { (error:NSError!) -> Void in
-//                print(error)
-//        }
-
-        api.getHomeTimelineSinceID("", count: 20, successBlock: { (statuses:[AnyObject]!) -> Void in
-                print(statuses)
+    func getTweets() {
+        api.getHomeTimelineSinceID(nil, count: 20, successBlock: { (tweets:[AnyObject]!) -> Void in
+            for tweet in tweets {
+                let text = tweet["text"] as! String
+                print(text)
+            }
             }) { (error:NSError!) -> Void in
                 print(error)
         }
-
     }
-
 }
 
